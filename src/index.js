bc84d3905a7b0825d4d5beb184723908d900d055
MakSimKlim <83maxomus@mail.ru>
2024-12-29 16:07:51 +0500

    Add basename in BrouserRouter DONE

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <BrowserRouter>
+  // Чтобы исправить проблему отображения пустой страницы при запуске, нужно настроить BrowserRouter, 
+  // чтобы учитывать базовый путь приложения, нужно дописать basename в BrowserRouter 
+    <BrowserRouter basename="/react_routing"> 
       <App />
     </BrowserRouter>
   //</React.StrictMode>
